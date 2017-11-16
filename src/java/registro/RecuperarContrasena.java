721af609dee23fb294f0249f8e445dfe0aa94011
Yahir Trejo <yahir.trejo.g@gmail.com>
2017-11-15 21:19:26 -0600

    liberias

--- src/java/registro/RecuperarContrasena.java
+++ src/java/registro/RecuperarContrasena.java
@@ -1,19 +1,17 @@
+package registro;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package registro;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.*;
+import javax.mail.internet.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
--- src/java/registro/RecuperarContrasena2.java
+++ src/java/registro/RecuperarContrasena2.java
@@ -1,9 +1,10 @@
+package registro;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package registro;
 
 import java.io.IOException;
 import java.io.PrintWriter;
